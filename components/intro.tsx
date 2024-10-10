072bf220f9ad491dfdc13bc38f0235e032bbf4cf
brandonngjh <brandonng2210@gmail.com>
2024-10-10 15:48:16 +0800

    chore: update intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,27 +34,13 @@ export default function Intro() {
             <Image
               src={portraitImg}
               alt="Brandon portrait"
-              width="192"
-              height="192"
+              width="256"
+              height="256"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-48 w-48 rounded-full object-cover border-[0.3rem] border-white shadow-xl"
             />
           </motion.div>
-
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            {/* 👋 */}
-          </motion.span>
         </div>
       </div>
 
